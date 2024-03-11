55d98ce5d2ad6f1bf72cb83e7068ee41f53c93da
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-03-11 10:17:17 -0300

    Update
    Adição do Speed Insights.

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,13 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-BR">
       <Head />
       <body>
+        <SpeedInsights />
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
